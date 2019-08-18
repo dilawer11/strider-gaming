d3f6ba50a29a5677e05a2032e744b2b92e1d03c6
Dilawer Ahmed <dilawer11@gmail.com>
2019-08-18 22:11:16 +0500

    Add Navigation Guards

--- src/router.js
+++ src/router.js
@@ -1,17 +1,49 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import UserPage from './views/UserPage.vue'
+import Register from './views/Register.vue'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+// export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta:{
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta:{
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta:{
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/userpage',
+      name: 'userpage',
+      component: UserPage,
+      meta:{
+        requiresAuth: true
+      }
     },
     {
       path: '/about',
@@ -22,4 +54,16 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
+  
 })
+router.beforeEach((to,from,next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  if(requiresAuth && !currentUser){
+    next('/login')
+  }else{
+    next()
+  }
+})
+
+export default router
